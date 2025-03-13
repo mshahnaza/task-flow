18f93f8faa838e2ced2daa6f94b3d6b51d03b886
mshahnaza <mamirovasahnaza042@gmail.com>
2025-03-13 09:25:18 +0600

    task,category,comment entities added

---
+++ src/main/java/com/example/taskmanager/entities/Category.java
@@ -0,0 +1,19 @@
+package com.example.taskmanager.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+}
---
+++ src/main/java/com/example/taskmanager/entities/Comment.java
@@ -0,0 +1,15 @@
+package com.example.taskmanager.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+}
